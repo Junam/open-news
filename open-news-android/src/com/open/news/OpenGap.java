36476c16f6f959ad3fea5a72f8221e05f7f5cd29
Oren Cohen <junam@.(none)>
2011-07-28 23:14:34 +0300

    Junam: added android application

---
+++ open-news-android/src/com/open/news/OpenGap.java
@@ -0,0 +1,15 @@
+package com.open.news;
+
+import android.os.Bundle;
+
+import com.phonegap.DroidGap;
+
+public class OpenGap extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("oncreate");
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
